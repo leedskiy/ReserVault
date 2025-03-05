e15060275cc48da3f3ea489ad408c9ebf3d3913c
Illia Takhtamyshev <leedqvo@gmail.com>
2025-03-05 17:39:12 +0100

    init backend

---
+++ reservault/reservault-backend/src/main/java/io/leedsk1y/reservault_backend/ReservaultBackendApplication.java
@@ -0,0 +1,13 @@
+package io.leedsk1y.reservault_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaultBackendApplication.class, args);
+	}
+
+}
